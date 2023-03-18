0d6d59c2f11b080722607283bd0a99e2a51db6dc
Pantelis Broukos <pbroukos@gmail.com>
2023-03-18 13:07:34 +0200

    initial full upload

---
+++ .pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
+ C:/Users/Pantelis/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
+ C:/Users/Pantelis/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
+ C:/Users/Pantelis/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/Pantelis/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
